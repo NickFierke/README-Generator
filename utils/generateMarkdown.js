8d6c53bb72c46cb4ab41f74188296990d62c8d0d
Mike <nicholasfierke97@gmail.com>
2023-02-07 10:15:36 -0500

    added generate

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if (!license) return '';
 
@@ -15,8 +14,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
   if (!license) return '';
 
@@ -32,8 +30,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
   if (!license) return '';
 
@@ -47,7 +44,7 @@ This project is licensed under the ${link}.
 ${badge}`;
 }
 
-// TODO: Create a function to generate markdown for README
+
 const generateMarkdown = (data) => {
   return `
 # ${data.title}
